3d7a554beda62351842767a30bf2c078555373e1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-13 11:30:44 +0900

    testコードを追記

--- src/Render.test.js
+++ src/Render.test.js
@@ -6,6 +6,8 @@ import Render from "./Render";
 describe("Rendering", () => {
   it("Should render all the elements correctly", () => {
     render(<Render />);
-    screen.debug();
+    // screen.debug();
+    // screen.debug(screen.getAllByRole("heading"));
+    expect(screen.getByRole("heading")).toBeTruthy();
   });
 });
