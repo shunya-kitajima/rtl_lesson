45785ba398dd3194b130b7818a9830e641c60393
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-13 16:18:47 +0900

    propsに渡す関数を追加

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import "./App.css";
 import RenderInput from "./RenderInput";
 
 function App() {
+  const output = (text) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <RenderInput />
+        <RenderInput outputConsole={output} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
