647cb971d1e2eeffa31274e02b18fbfb1f27729f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-13 11:51:02 +0900

    span要素にtestidを追加

--- src/Render.js
+++ src/Render.js
@@ -8,7 +8,7 @@ const Render = () => {
       <button>Click1</button>
       <button>Click2</button>
       <p>Udemy</p>
-      <span>@React</span>
+      <span data-testid="span">@React</span>
     </div>
   );
 };
