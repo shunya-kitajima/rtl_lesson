dce5e8560f000945c1ba010a20f84cb48fc8ec76
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-13 11:52:25 +0900

    renderingのテストコードを追記

--- src/Render.test.js
+++ src/Render.test.js
@@ -9,5 +9,12 @@ describe("Rendering", () => {
     // screen.debug();
     // screen.debug(screen.getAllByRole("heading"));
     expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    // screen.debug(screen.getByText("Udemy"));
+    expect(screen.getByText("Udemy")).toBeTruthy();
+    expect(screen.queryByText("Udeeeemy")).toBeNull();
+    expect(screen.getByTestId("span")).toBeTruthy();
   });
 });
