22d94c6cb6ae62b8a4c6bbe1fd9d940f9421b19c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-19 21:18:23 +0900

    MockServerコンポーネントをインポート

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import RenderInput from "./RenderInput";
 import FrameworkList from "./FrameworkList";
 import UseEffectRender from "./UseEffectRender";
+import MockServer from "./MockServer";
 
 function App() {
   const output = (text) => {
@@ -30,6 +31,7 @@ function App() {
         <Counter />
         <RenderInput outputConsole={output} />
         <FrameworkList frameworks={data} />
+        <MockServer />
         <UseEffectRender />
         <p>
           Edit <code>src/App.js</code> and save to reload.
