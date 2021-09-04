542f098df552cb0b9f99c04be6dfb897f78969a8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-09-04 12:09:18 +0900

    ReduxAsyncコンポーネントを作成

---
+++ src/ReduxAsync.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  selectCount,
+  fetchDummy,
+} from "./features/customCounter/customCounterSlice";
+
+const ReduxAsync = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <span test-dataid="count-value">{count}</span>
+      <button onClick={() => dispatch(fetchDummy(5))}>FetchDummy</button>
+    </div>
+  );
+};
+
+export default ReduxAsync;
