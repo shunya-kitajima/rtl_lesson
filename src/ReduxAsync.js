35cb1ca655a24369daa5b825ae6729a0b53730e8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-09-04 15:17:06 +0900

    fetchJSONをインポート

--- src/ReduxAsync.js
+++ src/ReduxAsync.js
@@ -3,17 +3,22 @@ import { useSelector, useDispatch } from "react-redux";
 
 import {
   selectCount,
+  selectUsername,
   fetchDummy,
+  fetchJSON,
 } from "./features/customCounter/customCounterSlice";
 
 const ReduxAsync = () => {
   const count = useSelector(selectCount);
+  const username = useSelector(selectUsername);
   const dispatch = useDispatch();
 
   return (
     <div>
       <span data-testid="count-value">{count}</span>
       <button onClick={() => dispatch(fetchDummy(5))}>FetchDummy</button>
+      {username && <h1>{username}</h1>}
+      <button onClick={() => dispatch(fetchJSON())}>FetchJOSN</button>
     </div>
   );
 };
