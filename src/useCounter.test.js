4bce23578fa49112762a302ade210de4034c7241
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-09-12 21:33:14 +0900

    customhooksのテストコードを追加

---
+++ src/useCounter.test.js
@@ -0,0 +1,49 @@
+import { cleanup } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react-hooks";
+
+import { useCounter } from "./useCounter";
+
+afterEach(() => cleanup());
+
+describe("useCounter custom Hook", () => {
+  it("Should increment by 1", () => {
+    const { result } = renderHook(() => useCounter(3));
+    expect(result.current.count).toBe(3);
+    act(() => {
+      result.current.increment();
+    });
+    expect(result.current.count).toBe(4);
+  });
+  it("Should decrement by 1", () => {
+    const { result } = renderHook(() => useCounter(3));
+    expect(result.current.count).toBe(3);
+    act(() => {
+      result.current.decrement();
+    });
+    expect(result.current.count).toBe(2);
+  });
+  it("Should dobule the counter value", () => {
+    const { result } = renderHook(() => useCounter(3));
+    expect(result.current.count).toBe(3);
+    act(() => {
+      result.current.dobule();
+    });
+    expect(result.current.count).toBe(6);
+  });
+  it("Should triple the counter value", () => {
+    const { result } = renderHook(() => useCounter(3));
+    expect(result.current.count).toBe(3);
+    act(() => {
+      result.current.triple();
+    });
+    expect(result.current.count).toBe(9);
+  });
+  it("Should reset to zero", () => {
+    const { result } = renderHook(() => useCounter(3));
+    expect(result.current.count).toBe(3);
+    act(() => {
+      result.current.reset();
+    });
+    expect(result.current.count).toBe(0);
+  });
+});
